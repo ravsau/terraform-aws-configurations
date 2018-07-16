5ad8f991d52d042057def51cb9309a77fb606d98
Saurav Sharma <sauravsharma011@gmail.com>
2018-07-16 18:38:11 -0400

    updated the minimal ec2 file

---
+++ ec2/ec2-minimal.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  access_key = "ACCESS_KEY_HERE"
+  secret_key = "SECRET_KEY_HERE"
+  region     = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+}
